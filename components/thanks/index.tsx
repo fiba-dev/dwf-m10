d3e1cc8d5d3d534b7a147e336bbd25e4095ab961
fiba-dev <fibarrola06@gmail.com>
2022-05-22 18:22:43 -0400

    delte error to upload

--- components/thanks/index.tsx
+++ components/thanks/index.tsx
@@ -1,34 +1,24 @@
 import React, { useState, useEffect } from "react";
-
 import { useRouter } from "next/router";
-
 import { Root } from "./styled";
 
-import { BotonMobil, BotonNaranja } from "components/ui/buttons";
 import { Large, Subtitle, Title } from "components/ui/texts";
 import { getOrder } from "lib/hooks";
 
 export function ThanksWindows() {
 	const router = useRouter();
-	console.log("SOY ROUTER QUERY", router.query);
 	const [data, setData] = useState({} as any);
 	const externalReference = router.query["external_reference"];
-	console.log("SOY REFERENCE", externalReference);
 	useEffect(() => {
 		if (externalReference) {
 			const fetchData = async () => {
 				const res = await getOrder(externalReference);
-				console.log("SOY RES", res);
 				setData(res);
 			};
 			fetchData().catch(console.error);
 		}
 	}, [externalReference]);
-	console.log("SOY DATAAAAA", data);
-
-	let status = data ? data.data.status : "";
-	console.log("SOY STATUS", status);
-
+	let status = data.data ? data.data.status : "";
 	const estado =
 		status == "closed"
 			? "FUE EXITOSO"
